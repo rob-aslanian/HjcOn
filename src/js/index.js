73745b9e9348cb30f407cc41c53beb64490f0d6a
Robert <30272573+robertguitar20@users.noreply.github.com>
2018-07-14 19:32:47 +0400

    Update index.js
    Add Collapse

--- src/js/index.js
+++ src/js/index.js
@@ -20,6 +20,32 @@ $(function(){
                         $(this.hash).offset().top
         }, 1000);
     });
+    
+    /** Menu Collapse */
+    $('.collapse-btn').click(function(e){
+
+        const collapse = $('.collapse');
+
+        if($(window).resize(function(e) { 
+            if($(window).width() > 768){
+                collapse.fadeIn(10);
+            }
+            else{
+                collapse.fadeOut(0);
+            }
+        }))
+
+        if(!e.target.classList.contains('collapsed')){
+            e.target.classList.add('collapsed');
+            collapse.slideDown(350);
+        }
+        else{
+            e.target.classList.remove('collapsed');
+            collapse.slideUp(350);
+          
+        } 
+        
+    });
 
     // Your code here....
 
